2f94b7dd8ce4723402ae59f45d17e4e3a3a0f679
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2023-01-07 12:36:21 +0100

    `on_bio_stream_write` can use `IOBuffer`

--- src/ssl.jl
+++ src/ssl.jl
@@ -51,7 +51,7 @@ end
 
 function on_bio_stream_write(bio::BIO, in::Ptr{Cchar}, inlen::Cint)::Cint
     try
-        io = bio_get_data(bio)::TCPSocket
+        io = bio_get_data(bio)
         written = unsafe_write(io, in, inlen)
         return Cint(written)
     catch e
