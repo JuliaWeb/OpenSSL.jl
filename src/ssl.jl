4a14e7e40ffc09682dca4b0c0730c03a183fc728
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2023-01-07 11:49:52 +0100

    Add type asserts to `bio_get_data`

--- src/ssl.jl
+++ src/ssl.jl
@@ -35,7 +35,7 @@ bio_clear_flags(bio::BIO) = bio_set_flags(bio, 0x00)
 function on_bio_stream_read(bio::BIO, out::Ptr{Cchar}, outlen::Cint)
     try
         bio_clear_flags(bio)
-        io = bio_get_data(bio)
+        io = bio_get_data(bio)::TCPSocket
         n = bytesavailable(io)
         if n == 0
             bio_set_read_retry(bio)
@@ -52,7 +52,7 @@ end
 
 function on_bio_stream_write(bio::BIO, in::Ptr{Cchar}, inlen::Cint)::Cint
     try
-        io = bio_get_data(bio)
+        io = bio_get_data(bio)::TCPSocket
         written = unsafe_write(io, in, inlen)
         return Cint(written)
     catch e
@@ -639,7 +639,7 @@ function Base.close(ssl::SSLStream, shutdown::Bool=true)
         end
         finalize(ssl.ssl)
     end
-    return 
+    return
 end
 
 """
